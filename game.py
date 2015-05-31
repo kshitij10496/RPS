29e33f50ab05ee96fadd2ef24a13018b7f1fd1e1
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2015-06-01 00:13:03 +0530

    added Scoring system

--- game.py
+++ game.py
@@ -27,7 +27,7 @@ def judge(c_hand_item, u_hand_item):
 		elif u_hand_item == "Paper":
 			return "Tie"
 		else:
-			return "You wins !!!"
+			return "You win !!!"
 	else:
 		if u_hand_item == "Rock":
 			return "You win !!!"
@@ -36,12 +36,46 @@ def judge(c_hand_item, u_hand_item):
 		else:
 			return "Tie !!!"
 
-print "Choose: Rock", "Paper", "Scissors"
+user_score = 0
+computer_score = 0
+while 1:
+
+	print "Choose: Rock", "Paper", "Scissors"
 
 
-user_hand = raw_input()
+	user_hand = raw_input()
 
 
-computer_hand= computer_hand_item()
-print "Computer's hand :", computer_hand
-print judge(computer_hand,user_hand)
+	computer_hand= computer_hand_item()
+	print "Computer's hand :", computer_hand
+	decision = judge(computer_hand,user_hand)
+	print decision
+	if decision == "You win !!!":
+		user_score += 1
+	elif decision == "Computer wins !!!":
+		computer_score += 1
+	else:
+		user_score += 0.5
+		computer_score += 0.5
+
+	print "Play again: Yes / No"
+	choice = raw_input()
+	if choice == "No":
+		print "Final Scores"
+		print "User =", user_score, "Computer =",computer_score
+		if user_score > computer_score:
+			print "You win the game !"
+			break
+		elif user_score < computer_score:
+			print "You lost the game !"
+			break
+		else:
+			print "The scores are tied !!!"
+			print "Play a tie-breaker ?"	
+			second_choice = raw_input()
+			if second_choice == "No!":
+				break
+			else:
+				continue
+	else :
+		continue
